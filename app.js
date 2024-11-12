8e31896476cfcd62eb6b293058593750b63a801b
seunggyun <asg5450@gmail.com>
2024-11-12 10:22:03 +0900

    초기 세팅
    초기 세팅

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import tweetsRouter from "router/tweets.js";
+import authRouter from "router/auth.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+// React 등 모듈의 기본 port가 3000이기에 8080으로 설정
+app.listen(8080);
